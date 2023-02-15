f1b3c0b2450ffb71dc74f581580d0b258c3255b3
Ross Condie <rosscondie@gmail.com>
2023-02-15 17:45:36 +0100

    added save function to book repository

--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -2,3 +2,12 @@ from db.run_sql import run_sql
 
 from models.book import Book
 from models.author import Author
+import repositories.author_repository as author_repository
+
+def save(book):
+    sql = "INSERT INTO books (author_id, title, genre) VALUES (%s, %s, %s) RETURNING *"
+    values = [book.author.id, book.title, book.genre]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    book.id = id
+    return book
