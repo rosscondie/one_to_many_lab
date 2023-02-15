74d2e598bddafa5d7d42dd49577a629c50b58357
Ross Condie <rosscondie@gmail.com>
2023-02-15 18:10:53 +0100

    added save function to author repository

--- repositories/author_repository.py
+++ repositories/author_repository.py
@@ -3,3 +3,10 @@ from db.run_sql import run_sql
 from models.author import Author
 from models.book import Book
 
+def save(author):
+    sql = "INSERT INTO authors (first_name, last_name) VALUES (%s, %s, %s) RETURNING *"
+    values = [author.first_name, author.last_name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    author.id = id
+    return author
