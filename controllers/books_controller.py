c4af567a58630c4d3938a938cb5becfeaeda8050
Ross Condie <rosscondie@gmail.com>
2023-02-16 08:13:48 +0100

    populated seeds file with data to display books

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from repositories import book_repository, author_repository
 from models.book import Book
-from models.author import Author
+
 
 from flask import Blueprint
 
@@ -9,9 +9,21 @@ books_blueprint = Blueprint("books", __name__)
 
 # RESTful CRUD Routes
 
+# INDEX
+# GET '/books'
+@books_blueprint.route("/books")
+def books():
+    books = book_repository.select_all()
+    return render_template("books/index.html", all_books = books)
+
 # NEW
 # GET '/books/new'
 # Return an HTML form to the browser for us to fill in
+@books_blueprint.route("/books/new")
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", all_authors = authors)
+
 
 # CREATE 
 # POST '/books'
--- models/book.py
+++ models/book.py
@@ -5,5 +5,5 @@ class Book:
         self.genre = genre
         self.id = id
 
-    def __repr__(self):
-        return f"author: {self.author}\ntitle: {self.title}\ngenre: {self.genre}\nid: {self.id}"
+    # def __repr__(self):
+    #     return f"author: {self.author.first_name}\ntitle: {self.title}\ngenre: {self.genre}\nid: {self.id}"
